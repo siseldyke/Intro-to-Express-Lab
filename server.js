c7ebb6974be78ac27d3f345105258a1a66acbbab
siseldyke <siseldyke@gmail.com>
2024-10-07 18:05:30 -0400

    everything except last function working

--- server.js
+++ server.js
@@ -14,8 +14,17 @@ const guitars = [
     { title: 'Martin D-15 Acoustic', price: 600 },
     { title: 'Fender Telecaster', price: 1000 }
   ];
-console.log(guitars)
 
+  const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+// console.log(shoes)
 app.get('/', (req, res) => {
     res.send(`Greetings`)
 })
@@ -29,20 +38,41 @@ app.get('/greetings', (req, res) =>{
 })
 
 app.get('/roll', (req, res) =>{
+    let number = req.query.number
+    let num = Number(number)
+    if(isNaN(num)){
+        return res.send('Please specify a number')
+    }
+    
+    let randomNumber = Math.floor(Math.random() * (num + 1))
     res.send({
-        msg: `You rolled a ${req.query.number}`
+        msg: `You rolled a ${randomNumber}`
+        
      })
     
 })
 
-app.get('/guitars', (req, res) =>{
+app.get('/guitars/:index', (req, res) =>{
+    let index = req.params.index
+    console.log(guitars[index])
+    if(index > 3) {
+        return res.send({
+           msg:`We do not have that in stock, please choose another option`
+       })
+   }
+   console.log(req.params)
     res.send({
-        msg: `Would you like the ${guitars.title} it is ${guitars.price}`
+        msg: `Would you like the ${guitars[index].title} it is ${guitars[index].price}`
+        
      })
     
 })
 
-
+app.get('/shoes/' , (req, res) =>{
+let minPrice = 15
+let maxPrice = 1000
+let types = ['sandal' , 'sneaker', 'boot', 'heel']
+})
 
 
 
